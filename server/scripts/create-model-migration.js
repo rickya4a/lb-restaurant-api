3f0ed84a1c0362e2b09aac79c6d505a793070e12
Ricky Aditya <rickyda79@gmail.com>
2024-04-24 15:02:19 +0700

    update data seed

--- server/scripts/create-model-migration.js
+++ server/scripts/create-model-migration.js
@@ -10,5 +10,22 @@ const models = [
 
 ds.automigrate(models, function(err, result) {
   if (err) throw err;
-  ds.disconnect();
+  server.models.Item.create([
+    {
+      name: 'Salad',
+      price: 1000
+    },
+    {
+      name: 'Quesadilla',
+      price: 1500
+    },
+    {
+      name: 'Hamburger',
+      price: 900
+    }
+  ], function(err, res) {
+    if (err) throw err;
+
+    console.log('Models created: \n', res);
+  });
 });
